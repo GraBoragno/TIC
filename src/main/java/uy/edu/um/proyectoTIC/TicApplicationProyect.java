275c6efdb7bd9857090fb5bd035c74be6145f0fa
GraBoragno <graboragno2005@gmail.com>
2024-10-27 09:50:08 -0300

    agregue el addRoom, falta chequear que no se pase de la cantidad de rooms del cine

--- src/main/java/uy/edu/um/proyectoTIC/TicApplicationProyect.java
+++ src/main/java/uy/edu/um/proyectoTIC/TicApplicationProyect.java
@@ -9,10 +9,12 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Component;
 import uy.edu.um.proyectoTIC.entities.users.Client;
 import uy.edu.um.proyectoTIC.exceptions.DuplicateEntityException;
+import uy.edu.um.proyectoTIC.exceptions.EntityNotFoundException;
 import uy.edu.um.proyectoTIC.repository.ClientRepository;
 import uy.edu.um.proyectoTIC.services.CinemaService;
 import uy.edu.um.proyectoTIC.services.ClientService;
 import uy.edu.um.proyectoTIC.services.FilmService;
+import uy.edu.um.proyectoTIC.services.RoomService;
 
 import java.time.LocalDate;
 
@@ -31,6 +33,9 @@ public class TicApplicationProyect {
 	@Autowired
 	private CinemaService cinemaService;
 
+	@Autowired
+	private RoomService roomService;
+
 	public static void main(String[] args) throws DuplicateEntityException {
 		ApplicationContext ctx = SpringApplication.run(TicApplicationProyect.class, args);
 		TicApplicationProyect app = ctx.getBean(TicApplicationProyect.class);
@@ -40,22 +45,33 @@ public class TicApplicationProyect {
 	public void runInCommandLine() throws DuplicateEntityException {
 		Client newClient = clientService.addClient("email@email.com","persona1","xd", "21/10/2024","1234");
 
-//		filmService.addFilm("Dune", "Denis Villeneuve", 155, "2021", "ciencia ficción, aventura");
-//		filmService.addFilm("Dune: Part Two", "Denis Villeneuve", 160, "2023", "ciencia ficción, aventura");
-//		filmService.addFilm("Inception", "Christopher Nolan", 148, "2010", "cie ncia ficción, thriller");
-//		filmService.addFilm("The Matrix", "Wachowski Sisters", 136, "1999", "acción, ciencia ficción");
-//		filmService.addFilm("Interstellar", "Christopher Nolan", 169, "2014", "ciencia ficción, drama");
-//
-//		cinemaService.addCinema(1, 8, "Punta Carretas");
-//		cinemaService.addCinema(2, 5, "Ciudad Vieja");
-//		cinemaService.addCinema(3, 7, "Pocitos");
-//		cinemaService.addCinema(4, 4, "Carrasco");
-//		cinemaService.addCinema(5, 6, "Tres Cruces");
-//		cinemaService.addCinema(6, 10, "Centro");
-//		cinemaService.addCinema(7, 3, "Malvín");
-//		cinemaService.addCinema(8, 6, "Buceo");
+		filmService.addFilm("Dune", "Denis Villeneuve", 155, "2021", "ciencia ficción, aventura");
+		filmService.addFilm("Dune: Part Two", "Denis Villeneuve", 160, "2023", "ciencia ficción, aventura");
+		filmService.addFilm("Inception", "Christopher Nolan", 148, "2010", "cie ncia ficción, thriller");
+		filmService.addFilm("The Matrix", "Wachowski Sisters", 136, "1999", "acción, ciencia ficción");
+		filmService.addFilm("Interstellar", "Christopher Nolan", 169, "2014", "ciencia ficción, drama");
 
-		System.out.println("Ejecutando programa...");
+		try {
+			cinemaService.addCinema(1, 8, "Punta Carretas");
+			cinemaService.addCinema(2, 5, "Ciudad Vieja");
+			cinemaService.addCinema(3, 7, "Pocitos");
+			cinemaService.addCinema(4, 4, "Carrasco");
+			cinemaService.addCinema(5, 6, "Tres Cruces");
+			cinemaService.addCinema(6, 10, "Centro");
+			cinemaService.addCinema(7, 3, "Malvín");
+			cinemaService.addCinema(8, 6, "Buceo");
+		} catch (DuplicateEntityException e) {
+			System.out.println("Error: Ya existe un cine con los mismos datos.");
+		}
+
+		try {
+			roomService.addRoom(1, 1);
+			roomService.addRoom(1, 10);
+		} catch (EntityNotFoundException e) {
+			System.out.println("Error: El cine no existe");
+        }
+
+        System.out.println("Ejecutando programa...");
 	}
 
 }
---
+++ src/main/java/uy/edu/um/proyectoTIC/services/RoomService.java
@@ -0,0 +1,39 @@
+package uy.edu.um.proyectoTIC.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import uy.edu.um.proyectoTIC.entities.Cinema;
+import uy.edu.um.proyectoTIC.entities.Room;
+import uy.edu.um.proyectoTIC.exceptions.EntityNotFoundException;
+import uy.edu.um.proyectoTIC.repository.CinemaRepository;
+import uy.edu.um.proyectoTIC.repository.RoomRepository;
+
+import java.util.Optional;
+
+@Service
+public class RoomService {
+
+    @Autowired
+    private RoomRepository roomRepository;
+
+    @Autowired
+    private CinemaRepository cinemaRepository;
+
+
+    public Room addRoom(Integer roomNbr, Integer centralId) throws EntityNotFoundException {
+
+        Optional<Cinema> result = cinemaRepository.findById((long) centralId);
+
+        if (result.isEmpty()){
+            throw new EntityNotFoundException("El cine no existe");
+        }
+
+        Room newRoom = Room.builder()
+                .roomNbr(roomNbr)
+                .cinemaRoom(result.get())
+                .build();
+
+        return roomRepository.save(newRoom);
+    }
+
+}
