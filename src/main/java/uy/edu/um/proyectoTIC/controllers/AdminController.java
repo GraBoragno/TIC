d095a873c07caddc685a8dd5e932debabd5acd4c
AgustinHUM <acayrus@correo.um.edu.uy>
2024-11-06 23:26:52 -0300

    added rest of admin services - Connected create snack and delete snack - el resto se puede basicamente copiar y pegar el formato, falta gets de los objetos

--- src/main/java/uy/edu/um/proyectoTIC/controllers/AdminController.java
+++ src/main/java/uy/edu/um/proyectoTIC/controllers/AdminController.java
@@ -1,5 +1,6 @@
 package uy.edu.um.proyectoTIC.controllers;
 
+import jakarta.persistence.EntityExistsException;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,29 +9,49 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import uy.edu.um.proyectoTIC.entities.Snack;
 import uy.edu.um.proyectoTIC.entities.users.Admin;
 import uy.edu.um.proyectoTIC.entities.users.Client;
+import uy.edu.um.proyectoTIC.exceptions.DuplicateEntityException;
 import uy.edu.um.proyectoTIC.exceptions.EntityNotFoundException;
+import uy.edu.um.proyectoTIC.services.AdminService;
 import uy.edu.um.proyectoTIC.services.ClientService;
 import uy.edu.um.proyectoTIC.services.FilmService;
+import uy.edu.um.proyectoTIC.services.SnackService;
+
+import java.util.List;
 
 @Controller
 public class AdminController {
 
+    @Autowired
+    private SnackService snackService;
+    private AdminService adminService;
+
+
+
     @GetMapping("/adminPage")
-    public String admin() {
+    public String admin(Model model) {
+
+        List<Snack> snacks = snackService.getAvailableSnacks();
+        model.addAttribute("snacks", snacks);
         return "adminPage";
     }
 
-//    @PostMapping("/test")
-//    public String test(Model model, HttpSession session) throws EntityNotFoundException
-//    {
-//        Client user = (Client) session.getAttribute("user");
-//        if (!isAdmin(user.getEmail())){
-//            return "redirect:/home";
-//        }
-//        return "redirect:/adminPage";
-//    }
+
+
+    @PostMapping("/adminAddSnack")
+    public String addSnack(@RequestParam String snackName, @RequestParam String snackPrice) throws DuplicateEntityException {
+        Snack newSnack = snackService.addSnack(snackName, snackPrice);
+        return "adminPage";
+
+    }
+
+    @PostMapping("/adminDeleteSnack")
+    public String deleteSnack(@RequestParam String snackId) {
+        return "adminPage";
+    }
 
 }
 
