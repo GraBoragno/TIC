cc35a6fe1009ba7aefe20fe199b2742f0a9761d7
AgustinHUM <acayrus@correo.um.edu.uy>
2024-11-08 18:14:54 -0300

    seats buttons

--- src/main/java/uy/edu/um/proyectoTIC/controllers/SeatController.java
+++ src/main/java/uy/edu/um/proyectoTIC/controllers/SeatController.java
@@ -2,12 +2,12 @@ package uy.edu.um.proyectoTIC.controllers;
 
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.converter.json.GsonBuilderUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+import uy.edu.um.proyectoTIC.entities.Broadcast;
+import uy.edu.um.proyectoTIC.entities.Seat;
 import uy.edu.um.proyectoTIC.entities.users.Client;
 import uy.edu.um.proyectoTIC.services.ClientService;
 import uy.edu.um.proyectoTIC.services.FilmService;
@@ -20,11 +20,16 @@ public class SeatController {
     private FilmService filmService;
 
     @GetMapping("/seats")
-    public String seats() {
+    public String seats(HttpSession session, Model model) {
 
         return "seats";
     }
 
+    @PostMapping("/selectSeat")
+    public String selectSeat(@RequestParam String seatId) {
+        System.out.println(seatId);
+        return "redirect:/payment";
+    }
 //    @PostMapping("/chooseSeats")
 //    public String chooseSeats(Model model, @RequestParam Long seatRow, @RequestParam Long seatColumn ,  @ModelAttribute(re)) {
 //        model.addAttribute("seatRow", seatRow);
